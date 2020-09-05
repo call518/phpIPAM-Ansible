9470f8ccf6a10af8e0d6f8bdecb8e0be74ad4409
JungJungIn <call518@gmail.com>
2020-09-05 05:43:27 -0700

    first completed

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ansible-playbook -i inventory.lst pb-dns-resolver.yml
+[ $? -eq 0 ] || exit 1
+
 ansible-playbook -i inventory.lst pb-timezone.yml
 [ $? -eq 0 ] || exit 1
 
@@ -13,5 +16,11 @@ ansible-playbook -i inventory.lst pb-common-pre.yml
 [ $? -eq 0 ] || exit 1
 
 ansible-playbook -i inventory.lst pb-mysql-server.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-httpd-php.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-phpIPAM.yml
 [ $? -eq 0 ] || exit 1
 
