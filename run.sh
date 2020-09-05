ad2c40b24a97a9aeb54efe7ef6c0de788fcabc99
JungJungIn <call518@gmail.com>
2020-09-05 02:47:40 -0700

    Initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ansible-playbook -i inventory.lst pb-timezone.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-firewall.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-chrony-ntp.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-common-pre.yml
+[ $? -eq 0 ] || exit 1
+
+ansible-playbook -i inventory.lst pb-mysql-server.yml
+[ $? -eq 0 ] || exit 1
+
